03614adfa0cea91852c7322d926f0ff48300bace
Devansh Agarwal <devansh4m@gmail.com>
2021-05-20 00:18:08 +0530

    refactoring

--- types.tsx
+++ types.tsx
@@ -1,10 +1,9 @@
 export type RootStackParamList = {
   Root: undefined;
-  NotFound: undefined;
 };
 
 export type BottomTabParamList = {
-  HomeOne: undefined;
+  Home: undefined;
   Profile: undefined;
 };
 
