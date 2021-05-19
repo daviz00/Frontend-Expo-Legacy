90b00453dd84cc6f0fd70588bb51a8099d2f9939
Devansh Agarwal <devansh4m@gmail.com>
2021-05-20 01:08:34 +0530

    Testing Bottom Modal

---
+++ components/BottomSheet/CreateRoomModal.tsx
@@ -0,0 +1,111 @@
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Button from 'react-native-paper';
+import withModalProvider from './ModalProvider';
+
+const DynamicSnapPointExample = () => {
+  // state
+  const [count, setCount] = useState(0);
+  const [contentHeight, setContentHeight] = useState(0);
+
+  // hooks
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const { bottom: safeBottomArea } = useSafeAreaInsets();
+
+  // variables
+  const snapPoints = useMemo(() => [contentHeight], [contentHeight]);
+
+  // callbacks
+  const handleIncreaseContentPress = useCallback(() => {
+    setCount(state => state + 1);
+  }, []);
+  const handleDecreaseContentPress = useCallback(() => {
+    setCount(state => Math.max(state - 1, 0));
+  }, []);
+
+  const handlePresentPress = useCallback(() => {
+    bottomSheetRef.current?.present();
+  }, []);
+  const handleDismissPress = useCallback(() => {
+    bottomSheetRef.current?.dismiss();
+  }, []);
+  const handleOnLayout = useCallback(
+    ({
+      nativeEvent: {
+        layout: { height },
+      },
+    }) => {
+      setContentHeight(height);
+    },
+    []
+  );
+
+  // styles
+  const contentContainerStyle = useMemo(
+    () => ({
+      ...styles.contentContainerStyle,
+      paddingBottom: safeBottomArea,
+    }),
+    [safeBottomArea]
+  );
+  const emojiContainerStyle = useMemo(
+    () => ({
+      ...styles.emojiContainer,
+      height: 50 * count,
+    }),
+    [count]
+  );
+
+  // renders
+  return (
+    <View style={styles.container}>
+      <Button label="Present" onPress={handlePresentPress} />
+      {/* <Button label="Dismiss" onPress={handleDismissPress} /> */}
+      <BottomSheetModal ref={bottomSheetRef} snapPoints={snapPoints}>
+        <BottomSheetView
+          style={contentContainerStyle}
+          onLayout={handleOnLayout}
+        >
+          <Text style={styles.message}>
+            Could this sheet modal resize to its content height ?
+          </Text>
+          <View style={emojiContainerStyle}>
+            <Text style={styles.emoji}>😍</Text>
+          </View>
+          <Button label="Yes" onPress={handleIncreaseContentPress} />
+          <Button label="Maybe" onPress={handleDecreaseContentPress} />
+        </BottomSheetView>
+      </BottomSheetModal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  contentContainerStyle: {
+    paddingTop: 12,
+    paddingHorizontal: 24,
+    backgroundColor: 'white',
+  },
+  message: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 12,
+  },
+  emoji: {
+    fontSize: 156,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  emojiContainer: {
+    overflow: 'hidden',
+    justifyContent: 'center',
+  },
+});
+
+export default withModalProvider(DynamicSnapPointExample);
