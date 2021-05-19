db589bf2742ad22da51b70e659827ad032b0d5b7
Devansh Agarwal <devansh4m@gmail.com>
2021-05-20 00:06:56 +0530

    updated Button Styles

---
+++ components/Buttons/FollowButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import {Button} from 'react-native-paper';
+
+const FollowButton = () => {
+  return (
+      <View style={styles.btnView}>
+        <Button
+          mode="contained"
+          labelStyle={styles.labelStyle}
+          color="#71A1FF"
+          icon="account-plus"
+          style={styles.btn}>
+          Follow
+        </Button>
+      </View>
+    );
+};
+
+export default FollowButton;
+
+const styles = StyleSheet.create({
+  btnView: {
+    flex: 1,
+    marginTop: 30,
+    justifyContent: 'flex-end',
+    alignSelf: 'stretch',
+    marginHorizontal: 70,
+  },
+  btn: {
+    borderRadius: 15,
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingVertical: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    textTransform: 'capitalize',
+    minHeight: 24,
+  },
+});
