f3f00b8d8b1e1ffb06ceea3bffcc08af18a232a1
Devansh Agarwal <devansh4m@gmail.com>
2021-05-20 20:03:28 +0530

    Implemented Card Swipe UI

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,58 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import {Title} from 'react-native-paper';
 import SingleRoomCard from '../components/SingleRoom/SingleRoomCard';
+import SwipeCards from "react-native-swipe-cards-deck";
+
+function Card() {
+  return (
+    <SingleRoomCard />
+  );
+}
 
 export default function HomeScreen() {
+
+  const [cards, setCards] = useState();
+
+  // replace with real remote data fetching
+  useEffect(() => {
+    setTimeout(() => {
+      setCards([
+        {}, {},
+      ]);
+    }, 5000);
+  }, []);
+
+  function handleSwipe(card) {
+    return true;
+  }
+
+  function handleMaybe(card) {
+    return false;
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Title style={styles.title}>
         All Rooms
       </Title>
-      <SingleRoomCard />
+      <SwipeCards
+        cards={cards}
+        renderCard={(cardData) => <Card data={cardData} />}
+        keyExtractor={(cardData) => String(cardData.text)}
+        handleYup={handleSwipe}
+        handleNope={handleSwipe}
+        handleMaybe={handleMaybe}
+        showYup={false}
+        showNope={false}
+        showMaybe={false}
+        hasMaybeAction={false}
+        loop={true}
+        smoothTransition={false}
+        stack={false}
+        stackOffsetX={0}
+        stackOffsetY={0}
+        />
     </SafeAreaView>
   );
 }
@@ -23,14 +65,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#13120E',
   },
-  roomCard: {
-    marginVertical: 20,
-  },
+
   title: {
     fontSize: 28,
     fontWeight: 'bold',
     marginRight: 195,
-    marginBottom: 60,
+    marginBottom: -80,
     color: '#FFFFFF'
   },
 });
