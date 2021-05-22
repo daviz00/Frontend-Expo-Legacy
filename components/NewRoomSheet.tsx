575710bebeecbc6dcc5c89d4851fd4dddfa707fc
Devansh Agarwal <devansh4m@gmail.com>
2021-05-22 19:03:42 +0530

    updated Styles

--- components/NewRoomSheet.tsx
+++ components/NewRoomSheet.tsx
@@ -4,9 +4,10 @@ import {
   TouchableOpacity,
   TextInput,
   Text,
+  StyleSheet
 } from 'react-native';
 
-const NewRoomSheet = ({ styles }) => {
+const NewRoomSheet = () => {
   return (
     <>
       <View
@@ -44,5 +45,38 @@ const NewRoomSheet = ({ styles }) => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  button: {
+    borderRadius: 30,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+    height: Dimensions.get("screen").height / 19.31,
+    paddingHorizontal:Dimensions.get("screen").width / 32.75,
+    // width: Dimensions.get('screen').width / 3.02,
+  },
+  input: {
+    width: Dimensions.get("screen").width / 1.965 ,
+    padding: 10,
+    marginTop: Dimensions.get("screen").height / 40.3,
+    marginBottom: Dimensions.get("screen").height / 80.6,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1.5,
+  },
+});
+
 
 export default NewRoomSheet;
--- screens/NewRoom.tsx
+++ screens/NewRoom.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useCallback } from "react";
-import { View, Button, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Button, Text, Dimensions } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import Home from "./HomeScreen";
@@ -46,34 +46,3 @@ export default function Example() {
   );
 }
 
-const styles = StyleSheet.create({
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    fontSize: 22,
-    textAlign: "center",
-    color: "#FFFFFF",
-  },
-  button: {
-    borderRadius: 30,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-    height: Dimensions.get("screen").height / 19.31,
-    paddingHorizontal:Dimensions.get("screen").width / 32.75,
-    // width: Dimensions.get('screen').width / 3.02,
-  },
-  input: {
-    width: Dimensions.get("screen").width / 1.965 ,
-    padding: 10,
-    marginTop: Dimensions.get("screen").height / 40.3,
-    marginBottom: Dimensions.get("screen").height / 80.6,
-    backgroundColor: "#fff",
-    borderBottomWidth: 1.5,
-  },
-});
